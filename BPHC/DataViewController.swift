3d85d0e91751fc9b130bad2fa0b519ae8522e53d
Berwald <Berwald@berwalds-macbook-pro.local>
2015-11-17 11:31:14 -0500

    Initial Commit

---
+++ BPHC/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  BPHC
+//
+//  Created by Chuanwei Zhou on 11/17/15.
+//  Copyright © 2015 Chuanwei Zhou. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
