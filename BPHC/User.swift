02e0d3b59ea6c266c59c608f664fc388df49de6d
connorgilheany <connorgilheany@gmail.com>
2016-02-04 16:56:56 -0500

    Created Profile and ProfileLogin view controllers. Created User data type Create Profile Login navbar, button Created modal view login page

---
+++ BPHC/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  BPHC
+//
+//  Created by Connor Gilheany on 2/4/16.
+//  Copyright © 2016 Chuanwei Zhou. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    //MARK: Properties
+    var username: String
+    
+    init(username: String){
+        self.username = username
+    }
+}
