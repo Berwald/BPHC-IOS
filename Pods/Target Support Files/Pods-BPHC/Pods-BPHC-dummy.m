df4d7d9a4f6f28adb489417897f46c4799b7e35a
ferrys <ferrys@bu.edu>
2016-04-03 17:14:29 -0400

    Fixed question to database

---
+++ Pods/Target Support Files/Pods-BPHC/Pods-BPHC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BPHC : NSObject
+@end
+@implementation PodsDummy_Pods_BPHC
+@end
---
+++ Pods/Target Support Files/Pods-BPHC/Pods-BPHC-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BPHCVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BPHCVersionString[];
+
